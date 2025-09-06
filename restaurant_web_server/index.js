f6297129af2bc4b35f3a0e27abb9527ff5c99652
Dachacho <arevadzedachi123@gmail.com>
2025-09-06 17:31:01 +0400

    added menu and operating hours data

--- restaurant_web_server/index.js
+++ restaurant_web_server/index.js
@@ -8,6 +8,14 @@ app.get("/", async (request, reply) => {
   return "Welcome to Fare is Fare!";
 });
 
+app.get("/menu", async (request, reply) => {
+  return "TODO: menu page";
+});
+
+app.get("/hours", async (request, reply) => {
+  return "TODO: hours page";
+});
+
 await app.listen({ port });
 
 console.log(`web server is listening at port: ${port}`);
