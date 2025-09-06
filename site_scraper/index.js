45cb5d79368ef806285bdc8fbc0493ab7e2dd28a
Dachacho <arevadzedachi123@gmail.com>
2025-09-07 00:15:24 +0400

    add readability with cheerio

--- site_scraper/index.js
+++ site_scraper/index.js
@@ -1,9 +1,15 @@
+import { load } from "cheerio";
+
 const URL = "https://medium.com/tag/nodejs";
 
 try {
   const response = await fetch(URL);
   const text = await response.text();
-  console.log(text);
+  const $ = load(text);
+  const elements = $("article h2");
+  elements.each((i, element) => {
+    console.log($(element).text());
+  });
 } catch (err) {
   console.log(err.message);
 }
