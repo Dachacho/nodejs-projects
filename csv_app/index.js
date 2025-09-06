6a577e29c4c34d11bb11bf7c3c199bdad8c4e50d
Dachacho <arevadzedachi123@gmail.com>
2025-09-06 14:24:18 +0400

    add person class with savetocsv function

--- csv_app/index.js
+++ csv_app/index.js
@@ -1,5 +1,24 @@
 import { createInterface } from "readline";
 import { promisify } from "util";
+import { appendFileSync } from "fs";
+
+class Person {
+  constructor(name = "", number = "", email = "") {
+    this.name = name;
+    this.number = number;
+    this.email = email;
+  }
+
+  saveToCSV() {
+    const content = `${this.name}, ${this.number}, ${this.email}`;
+    try {
+      appendFileSync("./contacts.csv", content);
+      console.log(`${this.name} added`);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+}
 
 const readline = createInterface({
   input: process.stdin,
@@ -7,14 +26,3 @@ const readline = createInterface({
 });
 
 const readLineAsync = promisify(readline.question).bind(readline);
-
-(async () => {
-  try {
-    const name = await readLineAsync("what is yourname: ");
-    console.log(`hello ${name}`);
-  } catch (err) {
-    console.log(err.message);
-  } finally {
-    readline.close();
-  }
-})();
